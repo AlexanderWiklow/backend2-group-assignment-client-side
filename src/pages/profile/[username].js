d04196ea2c812c0cc96e5fe755ba25978b165302
tsorak <karost0249@gmail.com>
2023-04-20 17:42:22 +0200

    Add follow button to profile page

--- src/pages/profile/[username].js
+++ src/pages/profile/[username].js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import Post from "@/components/post";
+import FollowButton from "@/components/followButton";
 
 export default function Profile() {
 	const [data, setData] = useState(null);
@@ -23,6 +24,9 @@ export default function Profile() {
 			<div className="flex flex-col items-center h-screen">
 				<main className="flex-grow w-full max-w-lg">
 					<h1 className="my-8 text-4xl text-center">{isLoading ? "..." : data?.username}</h1>
+					<div className="flex justify-end mb-4 mr-2" style={{ display: data?.clientIsProfileOwner ? "none" : "flex" }}>
+						<FollowButton clientIsFollowing={data?.clientIsFollowing || false} profileId={data?.id} />
+					</div>
 					<div className="border-b-2" />
 					{data?.posts ? data.posts.map((post, i) => <Post author={data.username} post={post} key={i} />) : null}
 				</main>
