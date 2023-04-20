d3ee7a1ef351c6ab2566d7a5662f3b55eecae2a2
AlexanderWiklow <a.wiklow@gmail.com>
2023-04-20 16:03:24 +0200

    add postlist component

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 import React, { useState } from "react";
 import { Inter } from "next/font/google";
+import PostList from "../components/PostList";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function Test() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Logged in view</h1>
+      <PostList />
     </main>
   );
 }
