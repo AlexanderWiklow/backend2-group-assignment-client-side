515950d45e4f8ccb5c3e088916c303f53c26b86f
tsorak <karost0249@gmail.com>
2023-04-22 16:43:31 +0200

    Add AppLayout component

---
+++ src/components/AppLayout.js
@@ -0,0 +1,23 @@
+export default function AppLayout({ Nav, Main, Sidebar, title }) {
+	return (
+		<div className="flex justify-center">
+			<div className="grid grid-cols-[minmax(auto,_16rem)_32rem_minmax(auto,_16rem)]">
+				<header>
+					<Nav />
+				</header>
+				<main className="flex flex-col w-full border-x-[1px] h-screen">
+					{title ? (
+						<>
+							<h1 className="m-2 text-2xl">{title}</h1>
+							<div className="border-b-2" />
+						</>
+					) : null}
+					<Main />
+				</main>
+				<aside>
+					<Sidebar />
+				</aside>
+			</div>
+		</div>
+	);
+}
