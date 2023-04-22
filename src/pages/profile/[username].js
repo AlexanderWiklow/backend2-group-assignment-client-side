a7d4050c1111de6efc3f33cca771a041fcfcc250
tsorak <karost0249@gmail.com>
2023-04-22 16:47:13 +0200

    Fix profile page
    - Implement using AppLayout component
    - Do fetch in a seperate function
    - Sort posts by date
    - Display PostCreate component if the client is the profile owner

--- src/pages/profile/[username].js
+++ src/pages/profile/[username].js
@@ -1,36 +1,51 @@
 import React, { useState, useEffect } from "react";
 
+import AppLayout from "@/components/AppLayout";
+import Nav from "@/components/Nav";
 import Post from "@/components/post";
 import FollowButton from "@/components/followButton";
+import PostCreate from "@/components/PostCreate";
 
 export default function Profile() {
+	return <AppLayout Nav={Nav} Main={Main} Sidebar={() => <></>} />;
+}
+
+function Main() {
 	const [data, setData] = useState(null);
 	const [isLoading, setLoading] = useState(false);
 
 	useEffect(() => {
 		setLoading(true);
-		fetch(`http://localhost:5050${location.pathname}`, {
-			credentials: "include"
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				setData(data);
-				setLoading(false);
-			});
+		fetchProfile();
 	}, []);
+
+	async function fetchProfile() {
+		const res = await fetch(`http://localhost:5050${location.pathname}`, {
+			credentials: "include"
+		});
+		const data = await res.json();
+		data.posts.sort((a, b) => {
+			const comparison = new Date(b.createdAt) - new Date(a.createdAt);
+			return comparison;
+		});
+		setData(data);
+		setLoading(false);
+	}
 
 	return (
 		<>
-			<div className="flex flex-col items-center h-screen">
-				<main className="flex-grow w-full max-w-lg">
-					<h1 className="my-8 text-4xl text-center">{isLoading ? "..." : data?.username}</h1>
-					<div className="flex justify-end mb-4 mr-2" style={{ display: data?.clientIsProfileOwner ? "none" : "flex" }}>
-						<FollowButton clientIsFollowing={data?.clientIsFollowing || false} profileId={data?.id} />
-					</div>
-					<div className="border-b-2" />
-					{data?.posts ? data.posts.map((post, i) => <Post author={data.username} post={post} key={i} />) : null}
-				</main>
+			<h1 className="m-2 text-3xl">{isLoading ? "..." : data?.username}</h1>
+			<div className="flex justify-end m-2" style={{ display: data?.clientIsProfileOwner ? "none" : "flex" }}>
+				<FollowButton clientIsFollowing={data?.clientIsFollowing || false} profileId={data?.id} />
 			</div>
+			<div className="border-b-2" />
+			<div style={{ display: data?.clientIsProfileOwner ? "block" : "none" }}>
+				<div className="p-2">
+					<PostCreate />
+				</div>
+				<div className="border-b-[1px]" />
+			</div>
+			<div className="flex flex-col flex-grow h-0 overflow-auto">{data?.posts ? data.posts.map((post, i) => <Post author={data.username} post={post} key={i} />) : null}</div>
 		</>
 	);
 }
