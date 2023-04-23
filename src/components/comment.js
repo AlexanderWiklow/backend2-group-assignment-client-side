208d3d398fc86314a6c29547c1de9e5a1b436d5c
June Zhao <110632376+juneyunyun@users.noreply.github.com>
2023-04-23 12:08:51 +0200

    uppdate Comment component to accept props

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,8 +1,10 @@
-export default function Comment() {
-	return (
-		<div>
-			<p>Comment author</p>
-			<p>Comment content</p>
-		</div>
-	);
+export default function Comment({ sender, comment }) {
+  return (
+    <div className="block">
+      <div className="bg-gray-100 m-0.5 inline-block px-2 rounded-2xl max-w-full break-words">
+        <p className="font-semibold">{sender}</p>
+        <p>{comment}</p>
+      </div>
+    </div>
+  );
 }
