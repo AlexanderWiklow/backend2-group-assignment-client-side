7b671f3cd23b85a7ce818ee3719804b64eb54d3b
tsorak <karost0249@gmail.com>
2023-04-24 11:29:15 +0200

    Use AppLayout on homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,11 +5,25 @@ import PostList from "../components/PostList";
 
 const inter = Inter({ subsets: ["latin"] });
 
+import AppLayout from "@/components/AppLayout";
+import Nav from "@/components/Nav";
+
 export default function Home() {
+  return <AppLayout Nav={Nav} Main={Main} Sidebar={Sidebar} title={"Home"} />;
+}
+
+function Main() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <h1>Logged in view</h1>
-      <PostList />
     </main>
+  );
+}
+
+function Sidebar() {
+  return (
+    <div className="mt-16">
+      <PostList />
+    </div>
   );
 }
