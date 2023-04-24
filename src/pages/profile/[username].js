ee51c1d3d30e27547fa4b844ca9f2e03b850bf7e
tsorak <karost0249@gmail.com>
2023-04-24 16:54:44 +0200

    Handle profile page fetch error
    - Display error in UI
    - Handle 404 error
    - Go back button using next/router

--- src/pages/profile/[username].js
+++ src/pages/profile/[username].js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 
+import { useRouter } from "next/router";
+
 import AppLayout from "@/components/AppLayout";
 import Nav from "@/components/Nav";
 import Post from "@/components/post";
@@ -11,8 +13,11 @@ export default function Profile() {
 }
 
 function Main() {
+	const router = useRouter();
+
 	const [data, setData] = useState(null);
 	const [isLoading, setLoading] = useState(false);
+	const [profileError, setProfileError] = useState(null);
 
 	useEffect(() => {
 		setLoading(true);
@@ -20,25 +25,46 @@ function Main() {
 	}, []);
 
 	async function fetchProfile() {
-		const res = await fetch(`http://localhost:5050${location.pathname}`, {
-			credentials: "include"
-		});
-		const data = await res.json();
-		data.posts.sort((a, b) => {
-			const comparison = new Date(b.createdAt) - new Date(a.createdAt);
-			return comparison;
-		});
-		setData(data);
+		try {
+			const res = await fetch(`http://localhost:5050${location.pathname}`, {
+				credentials: "include"
+			});
+			switch (res.status) {
+				case 404:
+					throw { err: "The specified profile does not exist." };
+				default:
+					break;
+			}
+
+			const data = await res.json();
+			data.posts.sort((a, b) => {
+				const comparison = new Date(b.createdAt) - new Date(a.createdAt);
+				return comparison;
+			});
+			setData(data);
+		} catch (error) {
+			console.error(error);
+			error.err ??= "Something went wrong.";
+			setProfileError(error.err);
+		}
 		setLoading(false);
 	}
 
 	return (
 		<>
 			<h1 className="m-2 text-2xl">{isLoading ? "..." : data?.username}</h1>
-			<div className="flex justify-end m-2" style={{ display: data?.clientIsProfileOwner ? "none" : "flex" }}>
+			<div className="flex justify-end m-2" style={{ display: data?.clientIsProfileOwner || profileError ? "none" : "flex" }}>
 				<FollowButton clientIsFollowing={data?.clientIsFollowing || false} profileId={data?.id} />
 			</div>
 			<div className="border-b-2" />
+			{profileError ? (
+				<>
+					<h2 className="my-8 text-xl text-center">{profileError}</h2>
+					<button className="text-center text-blue-400" onClick={() => router.back()}>
+						Go back
+					</button>
+				</>
+			) : null}
 			<div style={{ display: data?.clientIsProfileOwner ? "block" : "none" }}>
 				<div className="p-2">
 					<PostCreate />
